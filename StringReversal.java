0112079a36b7a24480d234b73705fa003e76eb2b
DivyaARamesh <62928667+DivyaARamesh@users.noreply.github.com>
2020-04-02 18:52:49 +0530

    String Reversal

---
+++ StringReversal.java
@@ -0,0 +1,20 @@
+package week1.day3;
+
+public class StringReversal {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String word= "Test Leaf";
+char[] array = word.toCharArray();
+System.out.println(array);
+for(int j=word.length()-1;j>=0;j--)
+
+{
+	System.out.print(array[j]);
+}
+	
+}
+}
+
+
